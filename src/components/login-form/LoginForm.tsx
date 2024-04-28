9554a4fe8c2eb8daf4666ce7197d3a9b0b8adc28
Jakub <jakub.wojciak2002@gmail.com>
2024-04-28 23:04:24 +0200

    Improved LoginForm with validation

--- src/components/login-form/LoginForm.tsx
+++ src/components/login-form/LoginForm.tsx
@@ -1,19 +1,68 @@
-import React from 'react';
-import { Button, TextField } from '@mui/material';
+import React, {useCallback, useMemo} from 'react';
+import {Button, TextField} from '@mui/material';
 import './LoginForm.css';
 import LoginIcon from '@mui/icons-material/Login';
+import {Formik} from 'formik';
+import * as yup from 'yup';
+
 
 function LoginForm() {
+    const onSubmit = useCallback(
+        (values: { username: string; password: string}, formik: any) => {console.log(values)}, []
+    );
+
+    const validationSchema = useMemo(() => yup.object().shape({
+            username: yup.string().required('Required'),
+            password: yup.string().required('Required').min(5, 'Password must be at least 5 characters'),
+        }), [],);
+
     return (
-        <div>
-            <form className="login-form" >
-                <TextField id="username" label="Username" variant="standard"/>
-                <TextField id="password" label="Password" variant="standard" type="password"/>
-                <Button variant="contained" startIcon={<LoginIcon />} type="submit">
-                    Sign in
-                </Button>
-            </form>
-        </div>
+        <Formik initialValues={{ username: '', password: '' }}
+                onSubmit={onSubmit}
+                validationSchema={validationSchema}
+                validateOnChange
+                validateOnBlur
+        >
+            {(formik: any) => (
+                <form
+                    className="login-form"
+                    id='signForm'
+                    onSubmit={formik.handleSubmit}
+                    noValidate
+                >
+                    <TextField
+                        id="username"
+                        label="Username"
+                        variant="standard"
+                        name="username"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.username && !!formik.errors.username}
+                        helperText={formik.touched.username && formik.errors.username}
+                    />
+                    <TextField
+                        id="password"
+                        label="Password"
+                        variant="standard"
+                        type="password"
+                        name="password"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.password && !!formik.errors.password}
+                        helperText={formik.touched.password && formik.errors.password}
+                    />
+                    <Button
+                        variant="contained"
+                        startIcon={<LoginIcon/>}
+                        type="submit"
+                        form="signForm"
+                        disabled={!formik.isValid || !formik.touched.password || !formik.touched.username}
+                    >
+                        Sign in
+                    </Button>
+                </form>
+            )}
+        </Formik>
     );
 }
 
