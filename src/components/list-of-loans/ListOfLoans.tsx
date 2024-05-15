32ea54eaf7353d3039623f09521cfa425b3a66f4
Jakub <jakub.wojciak2002@gmail.com>
2024-05-15 12:26:09 +0200

    Added prototype of loans page.

--- src/components/list-of-loans/ListOfLoans.tsx
+++ src/components/list-of-loans/ListOfLoans.tsx
@@ -1,5 +1,72 @@
+import React, {useState} from "react";
+import {loansData} from "../../res/loansData";
+import PersonIcon from "@mui/icons-material/Person";
+import TablePagination from "@mui/material/TablePagination";
+
 function ListOfLoans() {
-    return <h1>Loans</h1>;
+    const [searchTerm, setSearchTerm] = useState('');
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+
+    const handlePageChange = (
+        event: React.MouseEvent<HTMLButtonElement> | null,
+        newPage: number,
+    ) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const handleSearch = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setSearchTerm(event.target.value);
+    };
+
+    return (
+        <div className="listOfLoans">
+            <h1>List Of Loans </h1>
+            <table>
+                <thead>
+                <tr>
+                    <th>Loan ID</th>
+                    <th><PersonIcon className="PersonIcon"/> User </th>
+                    <th>Book</th>
+                    <th>Date of start</th>
+                    <th>Date of end</th>
+                    <th>Date of return</th>
+                </tr>
+                </thead>
+                <tbody>
+                {(rowsPerPage > 0
+                        ? loansData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        : loansData
+                ).map(loan => (
+                    <tr key={loan.loanId}>
+                        <td>{loan.user}</td>
+                        <td>{loan.book}</td>
+                        <td>{loan.dateOfStart}</td>
+                        <td>{loan.dateOfEnd}</td>
+                        <td>{loan.dateOfReturn}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            <TablePagination
+                component="div"
+                count={loansData.length}
+                page={page}
+                onPageChange={handlePageChange}
+                rowsPerPage={rowsPerPage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+        </div>
+    );
 }
 
 export default ListOfLoans;
---
+++ src/res/loansData.tsx
@@ -0,0 +1,90 @@
+export let loansData = [
+    {
+        loanId: 1,
+        book: 1,
+        user: 1,
+        dateOfStart: '2021-01-01',
+        dateOfEnd: '2021-03-01',
+        dateOfReturn: '2021-02-19'
+    },
+    {
+        loanId: 2,
+        book: 3,
+        user: 2,
+        dateOfStart: '2021-02-15',
+        dateOfEnd: '2021-04-15',
+        dateOfReturn: null
+    },
+    {
+        loanId: 3,
+        book: 8,
+        user: 5,
+        dateOfStart: '2021-03-10',
+        dateOfEnd: '2021-05-10',
+        dateOfReturn: '2021-05-10'
+    },
+    {
+        loanId: 4,
+        book: 12,
+        user: 8,
+        dateOfStart: '2021-04-05',
+        dateOfEnd: '2021-06-05',
+        dateOfReturn: null
+    },
+    {
+        loanId: 5,
+        book: 7,
+        user: 10,
+        dateOfStart: '2021-05-01',
+        dateOfEnd: '2021-07-01',
+        dateOfReturn: '2021-06-01'
+    },
+    {
+        loanId: 6,
+        book: 14,
+        user: 3,
+        dateOfStart: '2021-06-15',
+        dateOfEnd: '2021-08-15',
+        dateOfReturn: '2021-07-29'
+    },
+    {
+        loanId: 7,
+        book: 2,
+        user: 9,
+        dateOfStart: '2021-07-10',
+        dateOfEnd: '2021-09-10',
+        dateOfReturn: null
+    },
+    {
+        loanId: 8,
+        book: 11,
+        user: 4,
+        dateOfStart: '2021-08-05',
+        dateOfEnd: '2021-10-05',
+        dateOfReturn: null
+    },
+    {
+        loanId: 9,
+        book: 6,
+        user: 7,
+        dateOfStart: '2021-09-01',
+        dateOfEnd: '2021-11-01',
+        dateOfReturn: '2021-10-22'
+    },
+    {
+        loanId: 10,
+        book: 9,
+        user: 1,
+        dateOfStart: '2021-10-15',
+        dateOfEnd: '2021-12-15',
+        dateOfReturn: null
+    },
+    {
+        loanId: 11,
+        book: 16,
+        user: 6,
+        dateOfStart: '2021-11-10',
+        dateOfEnd: '2022-01-10',
+        dateOfReturn: null
+    }
+];
